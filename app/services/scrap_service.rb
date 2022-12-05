816e50f4e4347bfc69e92716801f0b2f3d5f71d9
HM Tanbir <tanbir2025@gmail.com>
2022-12-05 13:16:50 +0600

    test

--- app/services/scrap_service.rb
+++ app/services/scrap_service.rb
@@ -3,7 +3,7 @@
 require 'selenium-webdriver'
 
 class ScrapService
-  WAIT = Selenium::WebDriver::Wait.new(timeout: 120) # Maximum wait to find out search results html
+  WAIT = Selenium::WebDriver::Wait.new(timeout: 30) # Maximum wait to find out search results html
   # WEB_DRIVER = Selenium::WebDriver.for :firefox
 
   def self.call(url)
@@ -14,11 +14,11 @@ class ScrapService
     # Nokogiri::HTML(driver.page_source)
     client = Selenium::WebDriver::Remote::Http::Default.new
     options = Selenium::WebDriver::Firefox::Options.new(args: %w[--headless -disable-dev-shm-usage -no-sandbox])
-    client.read_timeout = 180 # seconds
+    client.read_timeout = 600 # seconds
     driver = Selenium::WebDriver.for(:firefox, capabilities: [options], http_client: client)
     WAIT.until { driver.get url }
-    # WAIT.until { driver.find_element(css: ".B_NuCI").displayed? }
+    #WAIT.until { driver.find_element(css: '.B_NuCI').displayed? }
     # driver.get url
-    Nokogiri::HTML(driver.page_source)
+    puts Nokogiri::HTML(driver.page_source)
   end
 end
